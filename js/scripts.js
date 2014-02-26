9d7299ca7c6967bb551a95b4ced417b7da77bfc5
epicodus <student@epicodus.com>
2014-02-25 17:17:40 -0800

    does not really work quite right

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 var Task = {
   displayTask: function() {
-    return this.taskName + ": " + this.taskDescription;
+    return this.priority + " " + this.taskName + ": " + this.taskDescription + " (" + this.dueDate + ")";
+
   }
 };
 
@@ -11,6 +12,16 @@ $(document).ready(function() {
   $("#add-task").click(function() {
     $("#new-set-of-tasks").append('<div class="new-task">' +
                                       '<div class="form-group">' +
+                                        '<label for="new-priority">Priority</label>' +
+                                        '<select class="form-control new-priority">' +
+                                          '<option value="1">1</option>' +
+                                          '<option value="2">2</option>' +
+                                          '<option value="3">3</option>' +
+                                          '<option value="4">4</option>' +
+                                          '<option value="5">5</option>' +
+                                        '</select>' +
+                                      '</div>' +
+                                      '<div class="form-group">' +
                                         '<label for="new-task-name">Task Name</label>' +
                                         '<input type="text" class="form-control new-task-name">' +
                                       '</div>' +
@@ -18,8 +29,13 @@ $(document).ready(function() {
                                         '<label for="new-task-description">Task Description</label>' +
                                         '<input type="text" class="form-control new-task-description">' +
                                       '</div>' +
+                                      '<div class="form-group">' +
+                                        '<label for="new-due-date">Due Date</label>' +
+                                        '<input type="text" class="form-control new-due-date">' +
+                                      '</div>' +
                                     '</div>');
   });
+  var currentList;
 
   $("form#new-list").submit(function(event) {
     event.preventDefault();
@@ -29,30 +45,43 @@ $(document).ready(function() {
     var newList = Object.create(List);
     newList.listName = inputtedListName;
     newList.tasks = [];
+    currentList = newList;
 
     $(".new-task").each(function() {
 
       var inputtedTaskName = $(this).find("input.new-task-name").val();
       var inputtedTaskDescription = $(this).find("input.new-task-description").val();
+      var inputtedPriority = $(this).find("select.new-priority").val();
+      var inputtedDueDate = $(this).find("input.new-due-date").val();
 
       var newTask = Object.create(Task);
       newTask.taskName = inputtedTaskName;
       newTask.taskDescription = inputtedTaskDescription;
+      newTask.isComplete = "(X)";
+      newTask.priority = inputtedPriority;
+      newTask.dueDate = inputtedDueDate;
 
-      newList.tasks.push(newTask);
+      currentList.tasks.push(newTask);
     });
     
-    $("ul#to-do-lists").append("<li><span class='clickable'>" + newList.listName + "</span></li>");
+    $("ul#to-do-lists").append("<li><span class='clickable-show'>" + newList.listName + "</span></li>");
 
-    $(".clickable").last().click(function() {
+    $(".clickable-show").last().click(function() {
+
+      currentList = newList;
       $("#show-list").show();
 
       $("#show-list h2").text(newList.listName);
 
       $("ul#all-tasks").text("");
       newList.tasks.forEach(function(task) {
-        $("ul#all-tasks").append("<li>" + task.displayTask() + "</li>")
+          $("ul#all-tasks").append("<li>" + task.displayTask() + " <span class='clickable-complete'>" + task.isComplete + "</span></li>")
       });
+
+      $(".clickable-complete").last().click(function() {
+        console.log(last.taskName);
+      });
+
     });
 
     this.reset();
